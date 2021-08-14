72ebb2cdd890721f6a3c3cbd734d998d49ababb8
LuisLobatoCzs <luis.lobatoczs@outlook.com>
2021-08-13 19:29:10 -0500

    Actualización index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,12 +15,18 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
 </head>
+
+<style>
+    body{
+        font-family: 'Montserrat', sans-serif;
+    }
+</style>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md bg-nb shadow-sm">
